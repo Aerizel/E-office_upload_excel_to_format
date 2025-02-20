b414bee72e77a4d66496fad66e787bbd7be5cac5
Pigeon <kritsakorn.s2002@gmail.com>
2025-02-20 09:32:28 +0700

    delete components on Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,19 @@
 "use client";
 import React from "react";
-import { useRouter } from "next/navigation";
 import { Menubar } from "primereact/menubar";
-import { MenuItem } from "primereact/menuitem";
 import Image from "next/image";
 import Link from "next/link";
 
 const start = (
   <div className="flex flex-row gap-2 justify-center items-center">
-    <Link href={"/"}>
+    <Link href={"/"} className="w-auto h-auto">
       <Image
         alt="logo"
         src="/ntLogo.jpg"
-        width="100"
-        height="100"
-        className="mr-2"
+        width={100}
+        height={100}
+        className="w-[100px] h-auto mr-2"
+        priority={true}
       />
     </Link>
     <h1 className="text-2xl font-bold">ระบบเเปลงเอกสาร</h1>
@@ -22,56 +21,6 @@ const start = (
 );
 
 export default function Navbar() {
-  const router = useRouter();
-  const items: MenuItem[] = [
-    {
-      label: "Home",
-      icon: "pi pi-home",
-      command: () => router.push("/"),
-    },
-    {
-      label: "Features",
-      icon: "pi pi-star",
-    },
-    {
-      label: "Projects",
-      icon: "pi pi-search",
-      items: [
-        {
-          label: "Upload",
-          icon: "pi pi-bolt",
-          command: () => router.push("/upload"),
-        },
-        {
-          label: "Blocks",
-          icon: "pi pi-server",
-        },
-        {
-          label: "UI Kit",
-          icon: "pi pi-pencil",
-        },
-        {
-          label: "Templates",
-          icon: "pi pi-palette",
-          items: [
-            {
-              label: "Apollo",
-              icon: "pi pi-palette",
-            },
-            {
-              label: "Ultima",
-              icon: "pi pi-palette",
-            },
-          ],
-        },
-      ],
-    },
-    {
-      label: "Contact",
-      icon: "pi pi-envelope",
-    },
-  ];
-
   return (
     <div className="card">
       <Menubar start={start} />
